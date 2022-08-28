740f86368d67048c02f51260facb254d9a8ee2e5
Mael-01 <83962099+Mael-01@users.noreply.github.com>
2022-08-28 18:42:37 -0300

    Delete imc.py

--- decisoes_encadeadas/imc.py
+++
@@ -1,6 +0,0 @@
-print('Preencha os campos abaixo e descubra seu IMC (Indice de Massa Corporal) ! Vamos lá !')
-peso=int(input('Digite seu peso (Kg)  :'))
-altura=float(input('Digite sua altura  (M) :'))
-print(altura/peso)
-
-
